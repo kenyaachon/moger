6bfde6f1c0f9ed1defdda4b3aa011ee866c87b6c
Moses Mbugua <swahili1963@gmail.com>
2023-10-24 23:30:25 -0700

    initial commands for moger cli tool

--- moger/moger/main.py
+++ moger/moger/main.py
@@ -4,8 +4,20 @@ app = typer.Typer()
 
 
 @app.command()
-def hello(name: str):
-    print(f"Hello {name}")
+def login(env: str):
+    print(f"logging into {env}")
+
+@app.command()
+def init(env: str):
+    print(f"Initalizing moger folder {env}")
+
+@app.command()
+def build(env: str):
+    print(f"Building application to env {env}")
+
+@app.command()
+def deploy(env: str):
+    print(f"Deploying application to env {env}")
 
 if __name__ == "__main__":
     app()
