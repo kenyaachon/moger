b1781ff364063aed7d2e40b9b02a071d1948be69
Moses Mbugua <swahili1963@gmail.com>
2023-10-29 15:36:19 -0700

    Initial github actions setup for running tests on push and pull requests - For tests, poetry will run pytest - For linting, poetry will run black and mypy

--- moger/main.py
+++ moger/main.py
@@ -4,20 +4,24 @@ app = typer.Typer()
 
 
 @app.command()
-def login(env: str):
+def login(env: str) -> None:
     print(f"logging into {env}")
 
+
 @app.command()
-def init(env: str):
+def init(env: str) -> None:
     print(f"Initalizing moger folder {env}")
 
-@app.command()
-def build(env: str):
-    print(f"Building application to env {env}")
 
 @app.command()
-def deploy(env: str):
+def build(env: str) -> None:
+    print(f"Building application to env {env}")
+
+
+@app.command()
+def deploy(env: str) -> None:
     print(f"Deploying application to env {env}")
+
 
 if __name__ == "__main__":
     app()
